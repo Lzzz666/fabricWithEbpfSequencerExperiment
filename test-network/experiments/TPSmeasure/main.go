c3dd0ef09b59205c30db3109e2bd61b0c420241e
Lzzz666 <zhenyuliu0825@gmail.com>
2026-03-06 14:57:50 +0800

    change tps measure time

--- test-network/experiments/TPSmeasure/main.go
+++ test-network/experiments/TPSmeasure/main.go
@@ -32,7 +32,7 @@ const (
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second) // Adjust duration here
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) // Adjust duration here
 	defer cancel()
 
 	// The gRPC client connection should be shared by all Gateway connections to this endpoint
