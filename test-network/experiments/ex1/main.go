64d6525cf213f7fc105c3acd4cf3a441eadd531c
Lzzz666 <zhenyuliu0825@gmail.com>
2026-07-11 00:40:21 +0800

    new ip

--- test-network/experiments/ex1/main.go
+++ test-network/experiments/ex1/main.go
@@ -21,8 +21,8 @@ import (
 
 var wgg sync.WaitGroup
 
-var peerEndpoints = [3]string{"10.140.0.25:13051", "10.140.0.29:14051", "10.140.0.29:14051"}
-var gatewayPeers = [3]string{"peer1.org1.example.com", "peer2.org1.example.com", "peer2.org1.example.com"}
+var peerEndpoints = [3]string{"10.140.0.28:14051", "10.140.0.25:13051", "10.140.0.29:14051"}
+var gatewayPeers = [3]string{"peer0.org1.example.com", "peer1.org1.example.com", "peer2.org1.example.com"}
 
 func main() {
 
